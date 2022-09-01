f5371853b98fb6beb3a4c3d672071f7f0e953a99
Jhin <im.jhin@gmail.com>
2022-09-01 20:07:13 -0300

    variables

---
+++ clase_1/variables.py
@@ -0,0 +1,14 @@
+# Variables
+
+mensaje_usuario = input("Ingrese un mensaje: ")
+print(mensaje_usuario)
+
+# Enteros, flotantes y operaciones int float string
+x = int(input('ingrese un numero: '))
+y = int(input('ingrese otro numero: '))
+# Para saber que tipo de variable es.
+print('El tipo de dato de x es: ', type(x))
+print('El tipo de dato de y es: ', type(y))
+
+suma = x * y
+print('El resultado es: ',suma)
