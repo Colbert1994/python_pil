93f7e9692621b7dce4b85afb6823b35377ffd700
Jhin <im.jhin@gmail.com>
2022-09-01 19:36:46 -0300

    entradasy salidas de datos

---
+++ clase_1/entrada_salida_datos.py
@@ -0,0 +1,20 @@
+# Salida de datos
+print('Hola\t mundo\n')
+# Otras funciones de print
+print(2 + 2)
+
+# Salto de linea
+print('Esto es un salto de \n de linea')
+
+# Tabulacion
+print('Esto es una \t tabulacion')
+
+#concatenar
+print('Hola','mundo',sep=' ')
+
+# End
+print('Hola', 'Mundo', end='==')
+print('Desde visual')
+
+# Entrada de datos
+input('Ingrese un mensaje: ')
